2b168dbf76b75ed7ffcb63b6edff41f4df259765
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-02-23 20:37:06 -0800

    starting on javascript

--- app.js
+++ app.js
@@ -1,6 +1,54 @@
 
-var pictureNames = []
+var productNames = ['bag','banana','bathroom','boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scrissors','shark','tauntaun','unicorn','water-can','wine-glass'];
+var productArr = [];
+
+// Constructor function
+function Product() {
+  this.name;
+  this.votes = votes;
+  this.path = path;
+  productArr.push(this)
+}
 
 function randomPic() {
   return Math.random()
 }
+
+(function() {
+
+})()
+
+var tracker = {
+}
+
+function newProducts() {
+  for (var i = 0; i < productNames.length; i++)
+  new Product(productNames[i], 'img/' + productNames + '.jpg');
+}
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~ */
+// 1. Generate three random, non-dupe images (part of the controller)
+// 2. Object constructor for Products:
+  // a. Include name, path, votes
+// 3. A tracker object that will controll functionality of app
+// 4. Event listener(s) for image clicks
+
+var productImageNames = ['bag', 'banana', '...'];
+var productsArray = [];
+
+function Products() {
+
+}
+
+// a simple IIFE to build all the product images
+(function() {
+  // build the objects
+})()
+
+var tracker = {
+  // lots of properties and methods
+}
+
+someEl.addEventListener('click', function(e) {
+  // does some stuff on click
