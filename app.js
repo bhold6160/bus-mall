10085d8ea9b855d9bb8044a6727d94d462bab665
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-02-25 15:34:59 -0800

    trying to get images to show

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 var productNames = ['bag','banana','bathroom','boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scrissors','shark','tauntaun','unicorn','water-can','wine-glass'];
+
 var productArr = [];
 
 // Constructor function
@@ -10,45 +11,50 @@ function Product() {
   productArr.push(this)
 }
 
+// Producing each new product
+// function newProducts() {
+//   console.log(productArr)
+//   for (var i = 0; i < productNames.length; i++)
+//   new Product(productNames[i], 'img/' + productNames + '.jpg');
+// }
+
+// Random number generator
 function randomPic() {
-  return Math.random()
+  return Math.floor(Math.random() * productNames.length);
 }
 
-(function() {
+function showPictures() {
+  console.log(showPictures);
+  var img1 = document.getElementById('img1');
+  var img2 = document.getElementById('img2');
+  var img3 = document.getElementById('img3');
 
-})()
+  img1.textContent = '';
+  img2.textContent = '';
+  img3.textContent = '';
 
-var tracker = {
+var pictureIndex = randomPic();
+img1.src = productArr[pictureIndex].path;
+img1.id = productArr[pictureIndex].name;
+productArr[pictureIndex];
+
+// var pictureIndex2 = randomPicsIndex();
+// while (pictureIndex2 === pictureIndex) {
+//   pictureIndex2 = randomPicsIndex();
+// }
+//
+// img2.src = productArr[pictureIndex2].path;
+// img2.src = productArr[pictureIndex2].name;
+// productArr[pictureIndex2];
+//
+// var pictureIndex3 = randomPicsIndex();
+// while (pictureIndex3 === pictureIndex2 || pictureIndex3 === pictureIndex) {
+//   pictureIndex3 = randomPicsIndex();
+// }
+// img3.src = productArr[pictureIndex3].path;
+// img3.id = productArr[pictureIndex3].name;
+// productArr[pictureIndex];
+
 }
 
-function newProducts() {
-  for (var i = 0; i < productNames.length; i++)
-  new Product(productNames[i], 'img/' + productNames + '.jpg');
-}
-
-
-/* ~~~~~~~~~~~~~~~~~~~~~~~~ */
-// 1. Generate three random, non-dupe images (part of the controller)
-// 2. Object constructor for Products:
-  // a. Include name, path, votes
-// 3. A tracker object that will controll functionality of app
-// 4. Event listener(s) for image clicks
-
-var productImageNames = ['bag', 'banana', '...'];
-var productsArray = [];
-
-function Products() {
-
-}
-
-// a simple IIFE to build all the product images
-(function() {
-  // build the objects
-})()
-
-var tracker = {
-  // lots of properties and methods
-}
-
-someEl.addEventListener('click', function(e) {
-  // does some stuff on click
+showPictures();
