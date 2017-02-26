aea309cfbdf709ad5330ba6f423daa2c7d8120f1
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-02-25 16:13:21 -0800

    all 3 images showing

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 
-var productNames = ['bag','banana','bathroom','boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scrissors','shark','tauntaun','unicorn','water-can','wine-glass'];
+var productNames = ['bag','banana','bathroom','boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','tauntaun','unicorn','water-can','wine-glass'];
 
 var productArr = [];
 
 // Constructor function
-function Product() {
-  this.name;
-  this.votes = votes;
+function Product(name, path) {
+  this.name = name;
+  this.votes = 0;
   this.path = path;
   productArr.push(this)
 }
 
 // Producing each new product
-// function newProducts() {
-//   console.log(productArr)
-//   for (var i = 0; i < productNames.length; i++)
-//   new Product(productNames[i], 'img/' + productNames + '.jpg');
-// }
+for (var i = 0; i < productNames.length; i++) {
+  new Product(productNames[i], 'img/' + productNames[i] + '.jpg');
+}
+console.log(productArr);
 
 // Random number generator
 function randomPic() {
+  console.log(randomPic)
   return Math.floor(Math.random() * productNames.length);
 }
 
+// Attaching pictures to the DOM
 function showPictures() {
   console.log(showPictures);
   var img1 = document.getElementById('img1');
@@ -38,22 +39,22 @@ img1.src = productArr[pictureIndex].path;
 img1.id = productArr[pictureIndex].name;
 productArr[pictureIndex];
 
-// var pictureIndex2 = randomPicsIndex();
-// while (pictureIndex2 === pictureIndex) {
-//   pictureIndex2 = randomPicsIndex();
-// }
-//
-// img2.src = productArr[pictureIndex2].path;
-// img2.src = productArr[pictureIndex2].name;
-// productArr[pictureIndex2];
-//
-// var pictureIndex3 = randomPicsIndex();
-// while (pictureIndex3 === pictureIndex2 || pictureIndex3 === pictureIndex) {
-//   pictureIndex3 = randomPicsIndex();
-// }
-// img3.src = productArr[pictureIndex3].path;
-// img3.id = productArr[pictureIndex3].name;
-// productArr[pictureIndex];
+var pictureIndex2 = randomPic();
+while (pictureIndex2 === pictureIndex) {
+  pictureIndex2 = randomPic();
+}
+
+img2.src = productArr[pictureIndex2].path;
+img2.id = productArr[pictureIndex2].name;
+productArr[pictureIndex2];
+
+var pictureIndex3 = randomPic();
+while (pictureIndex3 === pictureIndex2 || pictureIndex3 === pictureIndex) {
+  pictureIndex3 = randomPic();
+}
+img3.src = productArr[pictureIndex3].path;
+img3.id = productArr[pictureIndex3].name;
+productArr[pictureIndex];
 
 }
 
